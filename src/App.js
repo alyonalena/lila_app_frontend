bff76d21951c059f666adb2858b41b615dc52249
alyonalena <elenaflikema@yandex.ru>
2025-07-22 20:13:26 +0300

    small changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Layout, Typography, Row, Col, Button, Input } from 'antd'
+import { Layout, Typography, Row, Col, Tag, Input } from 'antd'
 import { UserOutlined, HeartOutlined, ShoppingCartOutlined, WechatWorkOutlined, SketchOutlined, MessageOutlined } from '@ant-design/icons'
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react'
@@ -9,31 +9,23 @@ import 'swiper/swiper-bundle.css'
 
 import { List, Space, Segmented } from 'antd'
 
-import Img1 from './pics/1.png'
-import Img2 from './pics/2.png'
-import Img3 from './pics/3.png'
-import Img4 from './pics/4.png'
-import Img5 from './pics/5.png'
-import Img6 from './pics/6.png'
-import Img7 from './pics/7.png'
-import Img8 from './pics/8.png'
-import Img9 from './pics/9.png'
-import Img10 from './pics/10.png'
-import Img11 from './pics/11.png'
-import Img12 from './pics/12.png'
+import Img1 from './pics/1.jpg'
+import Img2 from './pics/2.jpg'
+import Img3 from './pics/3.jpg'
+
 import 'antd/dist/reset.css'
 import './App.css'
 
-const { Text, Link } = Typography
+const { Text, Title } = Typography
 
 const data = Array.from({ length: 5 }).map((_, i) => ({
   href: 'https://ant.design',
   title: `Солнцезащитные очки Miu Miu`,
   avatar: `Img${i+1}`,
   description:
-    'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    'Описание товара от AI',
   content:
-    'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+    'Конструктивное описание товара',
 }));
 
 const IconText = ({ icon, text }) => (
@@ -45,66 +37,70 @@ const IconText = ({ icon, text }) => (
 
 
 const { Header, Footer, Content } = Layout
-const { Title } = Typography;
-
-const handleMenuClick = () => {alert('handleMenuClick')}
-const handleUserClick = () => {alert('handleUserClick')}
-const handleLikeClick = () => {alert('handleLikeClick')}
-
-const images = [
-  Img1, Img2, Img3, Img4, Img5, Img6, Img7, Img8, Img9, Img10, Img11, Img12
-];
 
 function App() {
-
-  // Image visibility state
-  const [visibleImages, setVisibleImages] = useState(Array(images.length).fill(true))
-
-  useEffect(() => {
-    // Helper to randomly toggle a state
-    function randomToggle(setter) {
-      setter(v => Math.random() > 0.5 ? !v : v)
-    }
-    // Set up interval for images
-    const imageInterval = setInterval(() => {
-      setVisibleImages(prev => prev.map(v => (Math.random() > 0.7 ? !v : v)))
-    }, 2000);
-    return () => {
-      clearInterval(imageInterval)
-    };
-  }, [])
 
   return (
     <Layout style={{ minHeight: '100vh' }}>
       <Header style={{ background: 'white', borderBottom: '1px solid #bec2cd', color: 'black', textAlign: 'center', padding: '0 16px' }}>
         <Row align="middle" justify="space-between" style={{ height: '64px' }}>
-          <Col style={{ display: 'flex', alignItems: 'center', gap: 16 }}>
-            {/*<Button type="text" onClick={handleMenuClick} icon={<MenuOutlined style={{ fontSize: 24 }} />} />*/}
-          </Col>
           <Col flex="auto" style={{ textAlign: 'center' }}>
             <Title level={1} style={{ margin: 0, color: 'black', lineHeight: '64px' }}>L ī l ā</Title>
-          </Col>
-          <Col style={{ display: 'flex', alignItems: 'center', gap: 16, justifyContent: 'flex-end' }}>
-            {/*<Button type="text" onClick={handleUserClick} icon={<HeartOutlined style={{ fontSize: 24 }} />} />
-            <Button type="text" onClick={handleLikeClick} icon={<ShoppingCartOutlined style={{ fontSize: 24 }} />} />
-            */}
           </Col>
         </Row>
       </Header>
       <Content style={{ padding: '16px 16px 70px 16px', display: 'flex', flexDirection: 'column'}}>
-        <Text type="secondary"><MessageOutlined /> Настроение дня: </Text>
-        <div 
+        <div style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}><Title level={4}>Настроение дня</Title></div>
+        <div
           style={{
             border: '1px solid #f0f0f0',
             borderRadius: '8px',
             backgroundColor: 'white',
             marginTop: '8px',
-            padding: '16px 16px 0',
+            padding: '16px 16px',
           }}
         >
-          <Title level={4}>песня Lana Del Ray - Gods & Monsters</Title>
+          <Text type="secondary"> ... свидание в платье с атмосферой песни Gods and Monsters Lana del Rey</Text>
         </div><br/>
-        <Text type="secondary"><SketchOutlined /> Подборки для вашего настроения: </Text>
+        <div style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}><Title level={4}>Деконструкция образа </Title></div>
+        <div>
+          <Title level={4}>
+            {
+              "Святость / Грех / Икона / Разрушение / Бархат / Золото / Тьма / Слезы / Вино / Голливудские холмы / Отчаянная красота / Увядание / Эротика / Меланхолия / Театральность"
+              .split("/")
+              .map(str => <Tag>{str}</Tag>)
+            }
+          </Title>
+        </div><br/>
+        <div style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}><Title level={4}>Эмоция</Title></div>
+        <div
+          style={{
+            border: '1px solid #f0f0f0',
+            borderRadius: '8px',
+            backgroundColor: 'white',
+            marginTop: '8px',
+            padding: '16px 16px',
+          }}
+        >
+          <Text type="secondary">Опасное очарование, уязвимая сила, трагический гламур, осознанная потерянность...</Text>
+        </div><br/>        
+        <div style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}><Title level={4}>Подборки для вашего настроения </Title></div>
         <div >
           <Swiper
             spaceBetween={50}
@@ -137,7 +133,46 @@ function App() {
                     padding: '8px 16px 0',
                   }}
                 >
-                  <Title level={4}>Королева декаданса</Title>
+                  <Title level={5}>Вариант 1: "Грешная Мадонна"<br/></Title> - контраст святости и греха                 
+                </div>}
+                dataSource={data}
+                renderItem={(item) => (
+                  <List.Item
+                    key={item.title}
+                    actions={[
+                      <IconText icon={HeartOutlined} text="Сохранить" key="list-vertical-star-o" />,
+                      <IconText icon={ShoppingCartOutlined} text="Добавить в корзину" key="list-vertical-like-o" />,
+                    ]}
+                    extra={         
+                      <div><img style={{maxWidth: '100%'}} src={Img1}/></div>
+                    }
+                  >
+                    <List.Item.Meta
+                      title={<a href={item.href}>{item.title}</a>}
+                      description={item.description}
+                    />
+                    {item.content}
+                  </List.Item>
+                )}
+              />
+            </SwiperSlide>
+            <SwiperSlide
+              style={{
+                border: '1px solid #f0f0f0',
+                borderRadius: '8px',
+                backgroundColor: 'white',
+                padding: '0',
+              }}
+            >
+              <List
+                itemLayout="vertical"
+                size="large"
+                header={<div
+                  style={{
+                    padding: '8px 16px 0',
+                  }}
+                >
+                  <Title level={5}>Вариант 2: "Готическая принцесса"</Title>
                 </div>}
                 dataSource={data}
                 renderItem={(item) => (
@@ -148,7 +183,7 @@ function App() {
                       <IconText icon={ShoppingCartOutlined} text="Добавить в корзину" key="list-vertical-like-o" />,
                     ]}
                     extra={
-                      <img src={Img2}/>
+                      <img style={{maxWidth: '100%'}} src={Img2}/>
                     }
                   >
                     <List.Item.Meta
@@ -176,7 +211,7 @@ function App() {
                     padding: '8px 16px 0',
                   }}
                 >
-                  <Title level={4}>Готическая принцесса</Title>
+                  <Title level={5}>Вариант 3 "Ещё один образ"</Title>
                 </div>}
                 dataSource={data}
                 renderItem={(item) => (
@@ -187,46 +222,7 @@ function App() {
                       <IconText icon={ShoppingCartOutlined} text="Добавить в корзину" key="list-vertical-like-o" />,
                     ]}
                     extra={
-                      <img src={Img2}/>
-                    }
-                  >
-                    <List.Item.Meta
-                      title={<a href={item.href}>{item.title}</a>}
-                      description={item.description}
-                    />
-                    {item.content}
-                  </List.Item>
-                )}
-              />
-            </SwiperSlide>
-            <SwiperSlide
-              style={{
-                border: '1px solid #f0f0f0',
-                borderRadius: '8px 8px 0 0',
-                backgroundColor: 'white',
-                padding: '0',
-              }}
-            >
-              <List
-                itemLayout="vertical"
-                size="large"
-                header={<div
-                  style={{
-                    padding: '8px 16px 0',
-                  }}
-                >
-                  <Title level={4}>Ещё один образ</Title>
-                </div>}
-                dataSource={data}
-                renderItem={(item) => (
-                  <List.Item
-                    key={item.title}
-                    actions={[
-                      <IconText icon={HeartOutlined} text="Сохранить" key="list-vertical-star-o" />,
-                      <IconText icon={ShoppingCartOutlined} text="Добавить в корзину" key="list-vertical-like-o" />,
-                    ]}
-                    extra={
-                      <img src={Img4}/>
+                      <img style={{maxWidth: '100%'}} src={Img3}/>
                     }
                   >
                     <List.Item.Meta
