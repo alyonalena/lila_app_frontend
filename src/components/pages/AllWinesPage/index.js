925534de33a7d2a53b7aac9ed3372c6240a4ae38
alyonalena <elenaflikema@yandex.ru>
2025-08-15 20:04:57 +0300

    sxw

--- src/components/pages/AllWinesPage/index.js
+++ src/components/pages/AllWinesPage/index.js
@@ -1,15 +1,28 @@
 import React from 'react'
+import { Card, Avatar, Divider, Typography } from 'antd'
+import { FR, IT, ES } from 'country-flag-icons/react/3x2'
 
-import { Card } from 'antd'
 import WineShortInfoList from '../../WineShortInfoList'
 import { allWines } from '../../../data'
 
+const { Text } = Typography
 
 function AllWinesPage() {
 
     return (
         <Card>
+            <Divider>
+                <Avatar shape="square" src={<FR title="France" className="..."/>} />
+            </Divider>
             <WineShortInfoList wineList={allWines} readOnly={false}/>
+            <Divider>
+                <Avatar shape="square" src={<IT title="Italy" className="..."/>} />
+            </Divider>
+            <Text>Данные не загружены</Text>
+            <Divider>
+                <Avatar shape="square" src={<ES title="Spane" className="..."/>} />
+            </Divider>
+            <Text>Данные не загружены</Text>
         </Card>
     )
 }
