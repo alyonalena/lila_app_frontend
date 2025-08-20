d50d65f604f0aefb48044b7cbb42e142afb6f486
alyonalena <elenaflikema@yandex.ru>
2025-08-20 20:47:36 +0300

    sxw

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
-import { WebAppProvider } from '@telegram-apps/sdk-react'
 
 import { init, miniApp } from '@telegram-apps/sdk'
 
@@ -24,8 +23,6 @@ initializeTelegramSDK()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <WebAppProvider>
-      <App />
-    </WebAppProvider>
+    <App />
   </StrictMode>,
 )
